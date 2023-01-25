308ad76096b9fa88c3a65df4fc5a6193bbe142ea
mdmdr8 <89672780+mdmdr8@users.noreply.github.com>
2023-01-26 00:09:18 +0900

    객체의 필드

--- src/java/chapter06/Example/Korean.java
+++ src/java/chapter06/Example/Korean.java
@@ -7,9 +7,9 @@ public class Korean {
     String ssn;
 
     //생성자 선언
-    public Korean(String n, String s) {
-        name = n;
-        ssn = s;
+    public Korean(String name, String ssn) {
+        this.name = name;
+        this.ssn = ssn;
     }
 
 }
