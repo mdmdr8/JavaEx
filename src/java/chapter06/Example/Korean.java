93b5506fdc24f8bf63bf8c6f14bcb39a5bd6f7f3
mdmdr8 <89672780+mdmdr8@users.noreply.github.com>
2023-01-26 00:07:47 +0900

    매개 변수명

---
+++ src/java/chapter06/Example/Korean.java
@@ -0,0 +1,15 @@
+package java.chapter06.Example;
+
+public class Korean {
+    //필드 선언
+    String nation = "대한민국";
+    String name;
+    String ssn;
+
+    //생성자 선언
+    public Korean(String n, String s) {
+        name = n;
+        ssn = s;
+    }
+
+}
---
+++ src/java/chapter06/Example/KoreanExample.java
@@ -0,0 +1,14 @@
+package java.chapter06.Example;
+
+public class KoreanExample {
+    public static void main(String[] args){
+        //객체 생성
+        Korean k1 = new Korean("박자바", "011225-1234567");
+
+        //객체 데이터 읽기
+        System.out.println("k1.nation : " + k1.nation);
+        System.out.println("k1.name : " + k1.name);
+        System.out.println("k1.ssn : " + k1.ssn);
+        System.out.println();
+    }
+}
