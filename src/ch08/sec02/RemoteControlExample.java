4ee946f9b3c9387bf58f124b2c59ca8ebbf28d41
mdmdr8 <89672780+mdmdr8@users.noreply.github.com>
2023-01-29 20:43:43 +0900

    구현 객체 교체

--- src/ch08/sec02/RemoteControlExample.java
+++ src/ch08/sec02/RemoteControlExample.java
@@ -8,7 +8,14 @@ public class RemoteControlExample {
 
         //또는
         //RemoteControl rc = new Television();
+
+        rc = new Audio();
+        rc.turnOn();
     }
 }
 //출력
 //TV를 켭니다.
+
+//출력
+//TV를 켭니다.
+//Audio를 켭니다.
