525e7aa66c12a1a6368782beb88309d8c529d039
mdmdr8 <89672780+mdmdr8@users.noreply.github.com>
2023-02-19 17:51:19 +0900

    제네릭 메소드

---
+++ src/ch13/sec03/exam01/Box.java
@@ -0,0 +1,17 @@
+package ch13.sec03.exam01;
+
+public class Box<T> {
+    //필드
+    private T t;
+
+    //getter
+    public T get(){
+        return t;
+    }
+
+    //setter
+    public void set(T t) {
+        this.t = t;
+    }
+
+}
