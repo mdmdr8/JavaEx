38d50e5bbd30f24deec8018488e38d16afd74c44
mdmdr8 <89672780+mdmdr8@users.noreply.github.com>
2023-02-20 23:15:58 +0900

    제한된 타입, 와일드카드 타입 파라미터

---
+++ src/ch13/sec05/GenericExample.java
@@ -0,0 +1,43 @@
+package ch13.sec05;
+
+public class GenericExample {
+    public static void main(String[] args){
+        //모든 사람이 신청 가능
+        Course.registerCourse1(new Applicant<Person>(new Person()));
+        Course.registerCourse1(new Applicant<Worker>(new Worker()));
+        Course.registerCourse1(new Applicant<Student>(new Student()));
+        Course.registerCourse1(new Applicant<HighStudent>(new HighStudent()));
+        Course.registerCourse1(new Applicant<MiddleStudent>(new MiddleStudent()));
+        System.out.println();
+
+        //학생만 신청 가능
+//        Course.registerCourse2(new Applicant<Person>(new Person()));    (x)
+//        Course.registerCourse2(new Applicant<Worker>(new Worker()));    (x)
+        Course.registerCourse2(new Applicant<Student>(new Student()));
+        Course.registerCourse2(new Applicant<HighStudent>(new HighStudent()));
+        Course.registerCourse2(new Applicant<MiddleStudent>(new MiddleStudent()));
+        System.out.println();
+
+        //직장인 및 일반인만 신청 가능
+        Course.registerCourse3(new Applicant<Person>(new Person()));
+        Course.registerCourse3(new Applicant<Worker>(new Worker()));
+//        Course.registerCourse3(new Applicant<Student>(new Student()));    (x)
+//        Course.registerCourse3(new Applicant<HighStudent>(new HighStudent()));    (x)
+//        Course.registerCourse3(new Applicant<MiddleStudent>(new MiddleStudent()));     (x)
+    }
+}
+
+
+//출력
+//Person이(가) Course1을 등록함
+//Worker이(가) Course1을 등록함
+//Student이(가) Course1을 등록함
+//HighStudent이(가) Course1을 등록함
+//MiddleStudent이(가) Course1을 등록함
+//
+//Student이(가) Course2을 등록함
+//HighStudent이(가) Course2을 등록함
+//MiddleStudent이(가) Course2을 등록함
+//
+//Person이(가) Course3을 등록함
+//Worker이(가) Course3을 등록함
