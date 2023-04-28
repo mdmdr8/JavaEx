27762a1bc7843d87b4702c789628b94413ba542b
mdmdr8 <89672780+mdmdr8@users.noreply.github.com>
2023-04-28 15:10:17 +0900

    1차원 배열 - 공 바꾸기2

--- src/codingtest/oneDimensionalArray/BallChange.java
+++ src/codingtest/oneDimensionalArray/BallChange.java
@@ -4,38 +4,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class BallChange {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer str = new StringTokenizer(br.readLine());
-
-        int n = Integer.parseInt(str.nextToken());
-        int m = Integer.parseInt(str.nextToken());
-        System.out.println(n);
-        System.out.println(m);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
         int[] arr = new int[n];
-        // 처음 바구니에 담긴 값
         for(int i=0; i<n; i++){
             arr[i] = i+1;
         }
-        //변경 값
-        for(int i=0; i<m; i++){
-            StringTokenizer str1 = new StringTokenizer(br.readLine());
 
-            int num1 = Integer.parseInt(str1.nextToken());
-            int num2 = Integer.parseInt(str1.nextToken());
+        for(int i=0; i<m; i++){
             int temp = 0;
+            int num1 = sc.nextInt();
+            int num2 = sc.nextInt();
 
             temp = arr[num1-1];
             arr[num1-1] = arr[num2-1];
             arr[num2-1] = temp;
-
         }
-        for(int number : arr){
-            System.out.print(number + " ");
+        sc.close();
+        for(int j=0; j<arr.length; j++){
+            System.out.print(arr[j] + " ");
         }
     }
 }
